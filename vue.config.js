b0047c35f86136872353c336918ff7f9a6a315ac
Shamil <cap.o4ividnost1@gmail.com>
2022-12-07 04:13:30 +0600

    a

--- vue.config.js
+++ vue.config.js
@@ -6,10 +6,12 @@ module.exports = defineConfig({
         fs: false,
       },
     },
+    publicPath: process.env.NODE_ENV === "production" ? "/waifu-browser/" : "/",
   },
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/waifu-browser/" : "/",
 });
 
 module.exports = {
-  publicPath: "/waifu-browser/",
+  publicPath: process.env.NODE_ENV === "production" ? "/waifu-browser/" : "/",
 };
